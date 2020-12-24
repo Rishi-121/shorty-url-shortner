c7c66bee1c90be646087251e23ca5bebebc9c4bb
Rishi-121 <dashrushikesh1121@gmail.com>
2020-12-24 19:52:23 +0530

    Fixed some issues

--- config/db.js
+++ config/db.js
@@ -2,12 +2,19 @@ const { connect } = require("mongoose");
 
 const db = async () => {
   try {
-    await connect(process.env.DB_URI, {
-      useCreateIndex: true,
-      useFindAndModify: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }).then(() => console.log("Connected to the database successfully!"));
+    await connect(
+      "mongodb+srv://dev:" +
+        process.env.MONGO_ATLAS_PASS +
+        "@cluster0.jbwfg.mongodb.net/" +
+        process.env.MONGO_ATLAS_DB +
+        "?retryWrites=true&w=majority",
+      {
+        useCreateIndex: true,
+        useFindAndModify: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    ).then(() => console.log("Connected to the database successfully!"));
   } catch (err) {
     console.log(err);
   }
