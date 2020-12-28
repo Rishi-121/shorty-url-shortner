af1db831fe4611cb6820c2cd6c74dcda4dae816d
Rishi-121 <dashrushikesh1121@gmail.com>
2020-12-28 11:35:25 +0530

    Optimized the url-config.js

--- config/url-config.js
+++ config/url-config.js
@@ -1,7 +1,3 @@
-let url_endpoint = `http://localhost:${process.env.PORT}/`;
-
-if (process.env.NODE_ENV === "production") {
-  url_endpoint = `https://shorty-url-shrinker.herokuapp.com/`;
-}
+const url_endpoint = process.env.NODE_ENV === "production" ? `https://shorty-url-shrinker.herokuapp.com/` : `http://localhost:${process.env.PORT}/`;
 
 module.exports = url_endpoint;
